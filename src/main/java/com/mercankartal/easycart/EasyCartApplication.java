0b4f9e6c0331302529ae3e3a3184ce46aca9b304
Mercan Kartal <mercankartal7@gmail.com>
2025-08-18 16:14:36 +0300

    EasyCart backend klasörü eklendi

---
+++ src/main/java/com/mercankartal/easycart/EasyCartApplication.java
@@ -0,0 +1,13 @@
+package com.mercankartal.easycart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyCartApplication.class, args);
+    }
+
+}
